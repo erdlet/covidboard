713c97d983b5665f9ac13bbf9d448c54f8eaa13a
Erdle, Tobias <tobi.erdle@gmail.com>
2021-04-23 16:36:00 +0200

    Add router for easier navigation in UI

--- src/main/java/de/erdlet/covistat/web/DashboardController.java
+++ src/main/java/de/erdlet/covistat/web/DashboardController.java
@@ -3,6 +3,7 @@ package de.erdlet.covistat.web;
 import javax.inject.Inject;
 import javax.mvc.Controller;
 import javax.mvc.Models;
+import javax.mvc.UriRef;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -16,11 +17,16 @@ public class DashboardController {
     Models models;
 
     @Inject
+    Router router;
+
+    @Inject
     APIClient arcGisClient;
 
     @GET
+    @UriRef("dashboard")
     public String index() {
         models.put("page", new DashboardPage(arcGisClient.fetchStatistics()));
+        models.put("router", router);
         return "index.mustache";
     }
 }
--- src/main/java/de/erdlet/covistat/web/ImpressAndPrivacyController.java
+++ src/main/java/de/erdlet/covistat/web/ImpressAndPrivacyController.java
@@ -1,5 +1,9 @@
 package de.erdlet.covistat.web;
+
+import javax.inject.Inject;
 import javax.mvc.Controller;
+import javax.mvc.Models;
+import javax.mvc.UriRef;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -8,15 +12,25 @@ import javax.ws.rs.core.Response;
 @Controller
 public class ImpressAndPrivacyController {
 
+    @Inject
+    Models models;
+
+    @Inject
+    Router router;
+
     @GET
     @Path("impress")
+    @UriRef("impress")
     public Response showImpress() {
+        models.put("router", router);
         return Response.ok("impress.mustache").build();
     }
 
     @GET
     @Path("privacy")
+    @UriRef("privacy")
     public Response showPrivacyDeclaration() {
+        models.put("router", router);
         return Response.ok("privacy.mustache").build();
     }
 }
