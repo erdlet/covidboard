da0c68a45826d6d4941dbf146a9ba81c83121928
Erdle, Tobias <tobi.erdle@gmail.com>
2021-05-05 15:07:20 +0200

    Use correct names for files

--- build-and-push-docker-images.sh
+++ build-and-push-docker-images.sh
@@ -3,3 +3,6 @@ LATEST_IMAGE="erdlet/covistat:latest"
 VERSIONED_IMAGE="erdlet/covistat:$(git rev-parse --short HEAD)"
 
 docker build -t $LATEST_IMAGE -t $VERSIONED_IMAGE .
+
+docker push $LATEST_IMAGE
+docker push $VERSIONED_IMAGE
